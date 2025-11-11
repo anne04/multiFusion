38283ee941fc09394853381596b2cba7b3c2afa1
anne04 <anne.06.cse@gmail.com>
2025-11-11 11:20:41 -0500

    Update run_multiFusion.py

--- run_multiFusion.py
+++ run_multiFusion.py
@@ -19,25 +19,24 @@ if __name__ == "__main__":
     evaluate it using k-fold cross validation, and save the logs. 
     """
     parser = argparse.ArgumentParser()
-    # =========================== must be provided ===============================
+    # =========================== input parameters ===============================
     parser.add_argument( '--training_data', type=str, default='Multimodal-Quiz/training_data_multiFusion.pkl', help='Path to training data')
-    parser.add_argument( '--model_name', type=str, default="3DmultiFusion_test", help='Provide a model name')
+    parser.add_argument( '--model_name', type=str, default="2DmultiFusion_test", help='Provide a model name')
+    parser.add_argument('--wandb_project_name', type=str, default='2Dmultimodal_fusion', help='provide a project name for wandb log generation')
     parser.add_argument( '--fold_count', type=int, default=5, help='Provide the total fold')
     parser.add_argument( '--kfold_info_file', type=str, default="Multimodal-Quiz/data_split_5fold.csv", help='Provide the file name with k fold info')
-    #=========================== default is set ======================================
-    parser.add_argument( '--conv_dimension', type=int, default=3)
+    parser.add_argument( '--conv_dimension', type=int, default=2, help='Set to 2 or 3, for running 2D or 3D convolution operation respectively for feature extraction')
     parser.add_argument( '--num_epoch', type=int, default=1000, help='Number of epochs or iterations for model training')
     parser.add_argument( '--model_path', type=str, default='model/', help='Path to save the model state') # We do not need this for output generation  
-    parser.add_argument( '--output_path', type=str, default='output/', help='Path to save the model state')
-    parser.add_argument( '--dropout', type=float, default=0)
-    parser.add_argument( '--batch_size', type=int, default=10)
-    parser.add_argument( '--lr_rate', type=float, default=0.001)
-    parser.add_argument( '--manual_seed', type=str, default='no', help='Use it for reproducible result')
-    parser.add_argument( '--seed', type=int, help='Use it for reproducible result')
-    parser.add_argument('--wandb_project_name', type=str, default='3Dmultimodal_fusion')
+    parser.add_argument( '--output_path', type=str, default='output/', help='Path to save the final performance reports and loss curves in csv format')
+    #parser.add_argument( '--dropout', type=float, default=0.5, help='set a dropout value')
+    parser.add_argument( '--batch_size', type=int, default=10, help='Set the minibatch size for model training')
+    parser.add_argument( '--lr_rate', type=float, default=0.001, help='Set the learning rate for model training.')
+    parser.add_argument( '--manual_seed', type=str, default='no', help='Set it to yes for reproducible result')
+    parser.add_argument( '--seed', type=int, help='Set it to some integer for reproducible result')
     #=========================== optional ======================================
-    parser.add_argument( '--load', type=int, default=0, help='Set 1 to load a previously saved model state')  
-    parser.add_argument( '--load_model_name', type=str, default='None' , help='Provide the model name that you want to reload')
+    #parser.add_argument( '--load', type=int, default=0, help='Set 1 to load a previously saved model state')  
+    #parser.add_argument( '--load_model_name', type=str, default='None' , help='Provide the model name that you want to reload')
     #============================================================================
     args = parser.parse_args() 
 
@@ -84,16 +83,7 @@ if __name__ == "__main__":
     # get metadata to add padding if slide is missing or row/column mismatch between samples
     metadata_adc, metadata_hbv, metadata_t2w = get_metadata(patient_vs_modality_vs_image)
     ## maximum [C, H, W] for each modality 
-    '''
-    In [8]: metadata_adc
-    Out[8]: [25, 128, 120] --> after first maxpool2d: [25, 64, 60] --> after 2nd maxpool2d [25, 32, 30]   
 
-    In [9]: metadata_hbv
-    Out[9]: [25, 128, 120] --> after first maxpool2d: [25, 64, 60] --> after 2nd maxpool2d [25, 32, 30]
-
-    In [10]: metadata_t2w
-    Out[10]: [25, 640, 640] --> after first maxpool2d: [25, 320, 320] --> after 2nd maxpool2d [25, 160, 160]   
-    '''
     fold_patient_BCR_timeBCR_prediction = defaultdict(list)
     fold_vs_c_index = defaultdict(list)
     print_model_flag = 1
@@ -127,6 +117,7 @@ if __name__ == "__main__":
 
         print_model_flag = 0
         print('*** testing now ***')
+        # send the model name to load it for evaluating test data
         c_index, prediction_order = test_multiFusion(args.model_name, test_tensor, k)
         # patient_order and prediction_order has same order
         for i in range(0, len(prediction_order)):
@@ -144,13 +135,12 @@ if __name__ == "__main__":
 
     ###########
     case_by_case_report = pd.DataFrame(fold_patient_BCR_timeBCR_prediction)
-    case_by_case_report.to_csv(args.output_path + '/'+ args.model_name +'_case_by_case_report.csv', index=False)
+    case_by_case_report.to_csv(args.output_path + '/'+ model_name +'_case_by_case_report.csv', index=False)
+    print('Saved at: ' + args.output_path + '/'+ model_name +'_case_by_case_report.csv')
 
     fold_vs_c_index_report = pd.DataFrame(fold_vs_c_index)
-    fold_vs_c_index_report.to_csv(args.output_path + '/'+ args.model_name +'_fold_vs_c_index_report.csv', index=False)
+    fold_vs_c_index_report.to_csv(args.output_path + '/'+ model_name +'_fold_vs_c_index_report.csv', index=False)
+    print('Saved at: ' + args.output_path + '/'+ model_name +'_fold_vs_c_index_report.csv')
 
-    
+    print('All done.')
 
-
-
-      
